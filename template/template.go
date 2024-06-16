44cf98a5c68bbc9ad077503c6702631e49d77524
Morten Linderud <morten@linderud.pw>
2024-06-16 16:07:14 +0200

    template: introduce package to help create TPMTPublic
    Signed-off-by: Morten Linderud <morten@linderud.pw>

---
+++ template/template.go
@@ -0,0 +1,75 @@
+package template
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rsa"
+
+	"github.com/google/go-tpm/tpm2"
+)
+
+func EcdsaToTPMTPublic(pubkey *ecdsa.PublicKey, sha tpm2.TPMAlgID) *tpm2.TPMTPublic {
+	var ecc tpm2.TPMECCCurve
+	switch pubkey.Curve {
+	case elliptic.P256():
+		ecc = tpm2.TPMECCNistP256
+	case elliptic.P384():
+		ecc = tpm2.TPMECCNistP384
+	case elliptic.P521():
+		ecc = tpm2.TPMECCNistP521
+	}
+	return &tpm2.TPMTPublic{
+		Type:    tpm2.TPMAlgECC,
+		NameAlg: sha,
+		ObjectAttributes: tpm2.TPMAObject{
+			SignEncrypt:  true,
+			UserWithAuth: true,
+			Decrypt:      true,
+		},
+		Parameters: tpm2.NewTPMUPublicParms(
+			tpm2.TPMAlgECC,
+			&tpm2.TPMSECCParms{
+				CurveID: ecc,
+				Scheme: tpm2.TPMTECCScheme{
+					Scheme: tpm2.TPMAlgNull,
+				},
+			},
+		),
+		Unique: tpm2.NewTPMUPublicID(
+			tpm2.TPMAlgECC,
+			&tpm2.TPMSECCPoint{
+				X: tpm2.TPM2BECCParameter{
+					Buffer: pubkey.X.FillBytes(make([]byte, 32)),
+				},
+				Y: tpm2.TPM2BECCParameter{
+					Buffer: pubkey.Y.FillBytes(make([]byte, 32)),
+				},
+			},
+		),
+	}
+}
+
+func RSAToTPMTPublic(pubkey *rsa.PublicKey, bits int) *tpm2.TPMTPublic {
+	return &tpm2.TPMTPublic{
+		Type:    tpm2.TPMAlgRSA,
+		NameAlg: tpm2.TPMAlgSHA256,
+		ObjectAttributes: tpm2.TPMAObject{
+			SignEncrypt:  true,
+			UserWithAuth: true,
+			Decrypt:      true,
+		},
+		Parameters: tpm2.NewTPMUPublicParms(
+			tpm2.TPMAlgRSA,
+			&tpm2.TPMSRSAParms{
+				Scheme: tpm2.TPMTRSAScheme{
+					Scheme: tpm2.TPMAlgNull,
+				},
+				KeyBits: tpm2.TPMKeyBits(bits),
+			},
+		),
+		Unique: tpm2.NewTPMUPublicID(
+			tpm2.TPMAlgRSA,
+			&tpm2.TPM2BPublicKeyRSA{Buffer: pubkey.N.Bytes()},
+		),
+	}
+}
