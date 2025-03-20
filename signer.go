ddfee244ccb25d1252c0048f5033ede16b9ebf1e
Val Packett <val@packett.cool>
2025-03-20 17:02:47 -0300

    signer: add TPMHandleSigner
    A crypto.Signer implementation for a manually loaded key, for applications
    that do not use LoadKeyWithParent or even the actual key files at all.
    
    Signed-off-by: Val Packett <val@packett.cool>

--- signer.go
+++ signer.go
@@ -82,3 +82,77 @@ func NewTPMKeySigner(k *TPMKey, ownerAuth func() ([]byte, error), tpm func() tra
 		auth:      auth,
 	}
 }
+
+// TPMHandleSigner implements the crypto.Signer interface for an already-loaded TPMKey
+type TPMHandleSigner struct {
+	tpm     transport.TPMCloser
+	pubKey  crypto.PublicKey
+	keyAlgo tpm2.TPMAlgID
+	keySize int
+	handle  handle
+}
+
+func (t *TPMHandleSigner) Public() crypto.PublicKey {
+	return t.pubKey
+}
+
+func (t *TPMHandleSigner) Sign(_ io.Reader, digest []byte, opts crypto.SignerOpts) ([]byte, error) {
+	var digestalg, signalg tpm2.TPMAlgID
+	var digestlength int
+
+	switch opts.HashFunc() {
+	case crypto.SHA256:
+		digestalg = tpm2.TPMAlgSHA256
+		digestlength = 32
+	case crypto.SHA384:
+		digestalg = tpm2.TPMAlgSHA384
+		digestlength = 48
+	case crypto.SHA512:
+		digestalg = tpm2.TPMAlgSHA512
+		digestlength = 64
+	default:
+		return nil, fmt.Errorf("%s is not a supported hashing algorithm", opts.HashFunc())
+	}
+
+	if len(digest) != digestlength {
+		return nil, fmt.Errorf("incorrect checksum length. expected %v got %v", digestlength, len(digest))
+	}
+
+	signalg = t.keyAlgo
+	if _, ok := opts.(*rsa.PSSOptions); ok {
+		if signalg != tpm2.TPMAlgRSA {
+			return nil, fmt.Errorf("Attempting to use PSSOptions with non-RSA (alg %x) key", signalg)
+		}
+		signalg = tpm2.TPMAlgRSAPSS
+	}
+
+	rsp, err := TPMSign(t.tpm, t.handle, digest, digestalg, t.keySize, signalg)
+	if err != nil {
+		return nil, err
+	}
+	return EncodeSignatureASN1(rsp)
+}
+
+func NewTPMHandleSigner(
+	tpm transport.TPMCloser,
+	pubKey crypto.PublicKey,
+	keyAlgo tpm2.TPMAlgID,
+	keySize int,
+	handle handle,
+) TPMHandleSigner {
+	return TPMHandleSigner{tpm, pubKey, keyAlgo, keySize, handle}
+}
+
+func NewTPMHandleSignerFromKey(
+	tpm transport.TPMCloser,
+	key *TPMKey,
+	handle handle,
+) TPMHandleSigner {
+	pk, err := key.PublicKey()
+	// This shouldn't happen!
+	if err != nil {
+		panic(fmt.Errorf("failed producing public: %v", err))
+	}
+
+	return TPMHandleSigner{tpm, pk, key.KeyAlgo(), key.KeySize(), handle}
+}
